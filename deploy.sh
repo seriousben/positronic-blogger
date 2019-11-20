32b7e92444b60a2bbb055f087584f6758f7a5de5
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2019-11-19 22:06:02 -0500

    fix deploy (#4)

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-docker run --rm -v "$PWD":/usr/local/go/src/github.com/seriousben/newsblur-to-hugo -w /usr/local/go/src/github.com/seriousben/newsblur-to-hugo golang:1.7.5-alpine ./build.sh
-docker build -t seriousben/newsblur-to-hugo:latest .
-docker push seriousben/newsblur-to-hugo:latest
-
-kubectl apply --force -f deploy.yml
