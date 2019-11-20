e3aad514e784a8ea4ae77a0dbe7cbddf7bebbeeb
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2019-11-19 22:05:31 -0500

    fix deploy

--- secrets.sh
+++ secrets.sh
@@ -10,6 +10,6 @@ echo -n "$NEWSBLUR_USERNAME" > .secrets/newsblur_username
 echo -n "$NEWSBLUR_PASSWORD" > .secrets/newsblur_password
 echo -n "$GITHUB_TOKEN" > .secrets/github_token
 
-kubectl create secret generic newsblur --from-file=username=.secrets/newsblur_username --from-file=password=.secrets/newsblur_password
+kubectl create secret generic newsblur --from-file=username=.secrets/newsblur_username --from-file=password=.secrets/newsblur_password --namespace=newsblur-to-hugo
 
-kubectl create secret generic github --from-file=token=.secrets/github_token
+kubectl create secret generic github --from-file=token=.secrets/github_token --namespace=newsblur-to-hugo
